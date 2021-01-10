2aedd73cdb3c3ea9f0020c1c278c825a87d0f64f
Arsalan Shah <manactric@gmail.com>
2021-01-11 00:10:47 +0500

    Update main.js.php

--- plugins/default/usbirthdate/main.js.php
+++ plugins/default/usbirthdate/main.js.php
@@ -2,7 +2,7 @@
 Ossn.register_callback('ossn', 'init', 'us_birthdate_main_js');
 function us_birthdate_main_js(){
 	$(document).ready(function(){
-		 if($('.ossn-profile-bottom').length){
+		 if($('.ossn-profile-bottom').length > 0 && $('input[name="birthdate"]').length > 0){
 				inp = $('input[name="birthdate"]').val();
 				sp  = inp.split('/');
 				nd  = sp[1]+'/'+sp[0]+'/'+sp[2];
